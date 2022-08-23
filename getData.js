ee0b207a52d7ec327f8c165341e1ed1c78b4243b
Vinbrade <autodace@gmail.com>
2022-08-23 12:12:53 +0530

    moved assets to views

--- getData.js
+++ getData.js
@@ -1,9 +1,9 @@
 const fs = require("fs");
-const easyData = JSON.parse(fs.readFileSync("public/assets/easy.json", "utf-8"));
-const mediumData = JSON.parse(fs.readFileSync("public/assets/medium.json", "utf-8"));
-const advancedData = JSON.parse(fs.readFileSync("public/assets/advanced.json", "utf-8"));
-const etsEssentialData = JSON.parse(fs.readFileSync("public/assets/ets_essential.json", "utf-8"));
-const etsAdvancedData = JSON.parse(fs.readFileSync("public/assets/ets_advanced.json", "utf-8"));
+const easyData = JSON.parse(fs.readFileSync("views/assets/easy.json", "utf-8"));
+const mediumData = JSON.parse(fs.readFileSync("views/assets/medium.json", "utf-8"));
+const advancedData = JSON.parse(fs.readFileSync("views/assets/advanced.json", "utf-8"));
+const etsEssentialData = JSON.parse(fs.readFileSync("views/assets/ets_essential.json", "utf-8"));
+const etsAdvancedData = JSON.parse(fs.readFileSync("views/assets/ets_advanced.json", "utf-8"));
 const data = [easyData, mediumData, advancedData, etsEssentialData, etsAdvancedData];
 
 module.exports = data;
--- index.js
+++ index.js
@@ -9,11 +9,17 @@ app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "/views"));
 app.use(express.static(path.join(__dirname, "/public")));
 
-app.get('/:difficulty', (req, res) => {
-    const difficulty = req.params.difficulty;
-    res.render("showSets",{difficulty, data});
+app.get("/:difficulty", (req, res) => {
+  const difficulty = req.params.difficulty;
+  res.render("showSets", { difficulty, data });
+});
+
+app.get("/json/:type", (req, res) => {
+    const options = {root: path.join(__dirname,"/views/assets")};
+    const type = req.params.type;
+    res.sendFile(type, options)
 })
 
-app.get('*', (req, res) => {
-    res.render("home", {data});
-})
+app.get("*", (req, res) => {
+  res.render("home", { data });
+});
