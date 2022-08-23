99b719339450b9fb99d606b3bf5f60e1de3c4beb
Vinbrade <autodace@gmail.com>
2022-08-23 15:38:43 +0530

    added search suggestions

---
+++ public/js/sClient.js
@@ -0,0 +1,24 @@
+function makeList(data) {
+    if(data == "") return false;
+    return words.filter((ele)=>{
+        if(ele.toLowerCase().startsWith(data)) return ele;
+    })
+}
+
+function results(value) {
+    res = document.getElementById("result");
+    res.innerHTML = '';
+    let resLis = [];
+    resLis = makeList(value);
+    let list = '';
+    if(resLis && resLis.length != 0) {
+        for (i=0; i<resLis.length; i++) {
+            list += '<li>' + resLis[i] + '</li>';
+        }
+        res.innerHTML = '<ul>' + list + '</ul>'
+    }
+}
+
+function queryRes(obj, ev) {
+    ev.preventDefault();
+}
